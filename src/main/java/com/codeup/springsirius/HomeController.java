af9da8e110c722bc3453dc80c220870fdb50ec74
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-04-18 00:10:59 -0500

    views

--- src/main/java/com/codeup/springsirius/HomeController.java
+++ src/main/java/com/codeup/springsirius/HomeController.java
@@ -4,13 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
     
-    @GetMapping("/")
-    @ResponseBody
-    public String landingPage() {
-        return "This is the landing page";
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String landingPage() {
+//        return "This is the landing page";
+//    }
+    
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
     
     
