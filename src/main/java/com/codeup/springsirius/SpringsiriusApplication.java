948a86db9f3fc259e9b11a885f4b34a23ebc7e23
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-04-17 19:21:15 -0500

    first comit

---
+++ src/main/java/com/codeup/springsirius/SpringsiriusApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springsirius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsiriusApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsiriusApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/codeup/springsirius/SpringsiriusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springsirius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsiriusApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
